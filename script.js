c286d78b88a2473f514147c0c5f63fb2ab6feb06
PoPuGaLiC <64127778+PoPuGaLiC@users.noreply.github.com>
2020-08-22 03:27:30 +0500

    Revert "Добавлено переключение иконок."
    This reverts commit a67fa3fe0e1fcc3ef66c9f2dad18eb3d7c8dd2b3.

--- script.js
+++
@@ -1,8 +0,0 @@
-for (img of document.getElementsByClassName('humans')){
-	img.onclick = function ActiveHuman(event){  
-	for (image of document.getElementsByClassName('humans')){
-    	image.style.borderColor = "#999999";
-    };
-    event.target.style.borderColor = "#EB4A1F";
-};
-}

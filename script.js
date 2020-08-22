a89f25c96fe52464dfbec885fb22ec9740119750
PoPuGaLiC <64127778+PoPuGaLiC@users.noreply.github.com>
2020-08-22 20:54:20 +0500

    Изменение описаний.
    Проблема с начальным значением.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 for (img of document.getElementsByClassName('humans')){
-	img.onclick = function ActiveHuman(event){  
+	img.onclick = function ActiveHuman(event){
+	let number = 0;
+	let position = 0;  
 	for (image of document.getElementsByClassName('humans')){
+		number++;
+		if (image == event.target){
+			position = number;
+		}
     	image.style.borderColor = "#999999";
     };
+    let activeNumber = 0;
+    for (cell of document.getElementsByClassName('gall')){
+    	activeNumber++;
+    	cell.style.display = "none";
+    	console.log(number);
+    	if(activeNumber == position){
+    		cell.style.display = "block";
+    	};
+    };
     event.target.style.borderColor = "#EB4A1F";
-};
+	};
 }

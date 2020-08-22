26aae2b661ddbc9fa6a3e9ce0474879f45927777
PoPuGaLiC <64127778+PoPuGaLiC@users.noreply.github.com>
2020-08-23 00:25:39 +0500

    Работает переключение.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-hide();
-view(0);
+let position = 0;
+
+switching(position);
+
 
 for (img of document.getElementsByClassName('humans')){
 	img.onclick = activeHuman;
@@ -7,13 +9,16 @@ for (img of document.getElementsByClassName('humans')){
 
 function activeHuman(event){
 	let humans = document.getElementsByClassName('humans');
-	hide();
 	for (i = 0; i < humans.length; i++){
-		if (humans[i] == event.target){
-			view(i);
-		}
+		if (humans[i] == event.target) position = i;
     };
+    switching(position);
 };
+
+function switching(n){
+	hide();
+	view(n);
+}
 
 function view(n){
 	document.getElementById("governance").children[n].style.borderColor = "#EB4A1F";
@@ -26,3 +31,29 @@ function hide(){
 	for (cell of document.getElementsByClassName('gall'))
 		cell.style.display = "none";
 };
+
+document.getElementsByClassName('gallery')[0].children[0].onclick = switchingLeft;
+document.getElementsByClassName('gallery')[0].children[2].onclick = switchingRight;
+
+function switchingLeft(){
+	position = ((4+position-1))%4;
+	switching(position);
+};
+
+function switchingRight(){
+	position = ((4+position+1))%4;
+	switching(position);
+};
+
+
+// (((4+position-1))%4)
+// for (arrow of document.getElementsByClassName("las la-chevron-right")){
+// 	arrow.onclick = rotate;
+// }
+
+// function rotate(event){
+// 	console.log("rot")
+// 	event.target.style.transform = 'rotate(' + 90 + 'deg)';
+// };
+
+

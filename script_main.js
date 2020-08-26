5a355f02e9a16e02bd158c887cfdba3a4b01d1cb
PoPuGaLiC <64127778+PoPuGaLiC@users.noreply.github.com>
2020-08-27 04:01:41 +0500

    Добавлено взаимодействие с интерфейсом на странице "Местоположение".

--- script_main.js
+++ script_main.js
@@ -45,30 +45,20 @@ function switchingRight(){
 	switchingPeople(position);
 };
 
-let arr = document.getElementsByClassName("list")[0];
 
-for (let i = 0; i < 4; i++){
-	arr.children[i*2].children[0].onclick = rotate;
+let i=0;
+
+for(let chevron of document.getElementsByClassName("la-chevron-right")){
+	chevron.number= i;
+	i++;
+	chevron.onclick = rotate;
 }
 
 function rotate(event){
-	let question = 1;
-	for (let i = 0; i < 4; i++){
-			if (arr.children[i*2].children[0] == event.target) question = i*2+1;
-    	};
 	if(event.target.style.transform == 'rotate(90deg)'){
 		event.target.style.transform = 'rotate(0deg)';
-		hideQuestion(question);
 	} else {
 		event.target.style.transform ='rotate(90deg)';
-		showQuestion(question);
-	}
-};
-
-function hideQuestion(question){
-	document.getElementsByClassName("list")[0].children[question].style.display = "none";
-};
-
-function showQuestion(question){
-	document.getElementsByClassName("list")[0].children[question].style.display = "block";
+	};
+	event.target.parentElement.nextElementSibling.hidden = !event.target.parentElement.nextElementSibling.hidden;
 };
